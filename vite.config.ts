ea7c1ae705e9213b3db3aba3969613e2d9e2e4fa
Aleksandr Kuznetsov <sahakuz03@gmail.com>
2023-08-20 17:34:04 +0500

    feat: add cert

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile";
 import svgr from "@honkhonk/vite-plugin-svgr";
+import fs from "fs";
 
 export default ({ mode }) => {
   // Load app-level env vars to node-level env vars.
@@ -11,6 +12,12 @@ export default ({ mode }) => {
 
   return defineConfig({
     plugins: [react(), svgr(), process?.env?.ODR ? viteSingleFile() : null],
+    server: {
+      https: {
+        key: fs.readFileSync("./.cert/key.pem"),
+        cert: fs.readFileSync("./.cert/cert.pem"),
+      },
+    },
     resolve: {
       alias: {
         src: path.resolve("src/"),
